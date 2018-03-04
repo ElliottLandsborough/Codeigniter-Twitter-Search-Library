dca94c90f1c0ea83636b105798ed66b6cd12e6d3
suryotrisongko <suryotrisongko@gmail.com>
2018-03-05 04:23:54 +0700

    add 3 functionality: - retrieve tweet mentions - post new tweet - reply tweet forked from ElliottLandsborough/Codeigniter-Twitter-Search-Library

--- Twitterapi.php
+++ Twitterapi.php
@@ -1,14 +1,3 @@
-# Codeigniter-Twitter-Rest-API-Library
-## Call Twitter 1.1 Rest API from your code igniter applications.
-### Requirements other than codeigniter
-* [Codeigniter-TwitterOAuth](https://github.com/MunGell/Codeigniter-TwitterOAuth) for searching v1.1 api
-
-### How it works
-1. Call Twitter 1.1 API and save the result into mysql database
-2. Example Controller shows how to get mentions and post/reply 
-
-#### Example controller (controllers/Twitterapi.php)
-```php
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
 * Example controller for TwitterRestApilib
@@ -56,15 +45,3 @@ class Twitterapi extends CI_Controller {
     }
 }
 ?>
-```
-#### mySQL
-```mysql
-CREATE TABLE IF NOT EXISTS `tweets` (
-  `id` varchar(255) NOT NULL PRIMARY KEY,
-  `created_at` varchar(255) NOT NULL,
-  `user_name` varchar(255) NOT NULL,
-  `user_screen_name` varchar(255) NOT NULL,
-  `text` varchar(255) NOT NULL,
-  `reply` varchar(255) NOT NULL
-) ;
-```
